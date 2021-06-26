023abf1a435f051ff682fa06ac8432576cdedc27
Talhabbasi <43692176+Talhabbasi@users.noreply.github.com>
2021-06-26 17:31:50 +0500

    code Pushed

---
+++ scripts.js
@@ -0,0 +1,28 @@
+const newYearDate=('1 jan 2022')
+const daysel=document.getElementById('days')
+const hoursel=document.getElementById('hours')
+const minele=document.getElementById('min')
+const secele=document.getElementById('sec')
+function timerSetter() {
+    const yearTime=  new Date(newYearDate)
+    const currenttime = new Date()
+
+
+    const tototSeconds=Math.floor(yearTime-currenttime)/1000;
+
+    const days=Math.floor(tototSeconds/3600/24)
+    const hour=Math.floor(tototSeconds/3600)%24
+    const min=Math.floor(tototSeconds/60)%60
+    const sec=Math.floor(tototSeconds)%60
+
+
+    
+    console.log(days ,hour,min ,sec)
+    
+    daysel.innerHTML=days;
+    hoursel.innerText=hour;
+    minele.innerText=min
+    secele.innerText=sec
+}
+timerSetter();
+setInterval(timerSetter,1000)
